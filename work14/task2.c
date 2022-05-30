916cc9fd13cc3c9fa5494b7952ea1563171d7bd5
L0u1Za <nigoluiza@mail.ru>
2022-05-30 17:09:12 +0300

    Lab14.

---
+++ work14/task2.c
@@ -0,0 +1,19 @@
+#include <signal.h>
+#include <stdio.h>
+
+void my_handler(int nsig) {
+    if (nsig == 2) {
+        printf("Received signal %d, CTRL+C pressed\n", nsig);
+    } else if (nsig == 3) {
+        printf("Received signal %d, CTRL+4 pressed\n", nsig);
+    }
+}
+
+int main(void) {
+  (void)signal(SIGINT, my_handler);
+  (void)signal(SIGQUIT, my_handler);
+
+  while(1);
+
+  return 0;
+}
